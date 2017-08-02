cf9fc265fe6f4944fd460a6b032490fd27150551
thomas <dui1cuo@126.com>
2017-08-02 10:47:18 +0800

    [Mod]minor fix

---
+++ New-Project/qulicai/AppDelegate/AppDelegate.m
@@ -0,0 +1,44 @@
+//
+//  AppDelegate.m
+//  qulicai
+//
+//  Created by admin on 2017/8/2.
+//  Copyright © 2017年 qurong. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Todo-New-Project
+
+
+new project
