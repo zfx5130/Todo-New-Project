8f4a476749f14ce2e319f6b07441805d68fb37ec
thomas <dui1cuo@126.com>
2017-08-02 14:09:12 +0800

    no message

--- New-Project/qulicai/Controllers/ViewController.m
+++ New-Project/qulicai/Controllers/ViewController.m
@@ -16,14 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"测试";
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
-
 
 @end
