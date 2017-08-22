50ed5720255370bb542f5e3adeb256da435c32bf
thomas <dui1cuo@126.com>
2017-08-22 21:49:44 +0800

    [Mod]minor fix

---
+++ New-Project/qulicai/Controllers/Pruduct/ProductBuyViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductBuyViewController.h
+//  qulicai
+//
+//  Created by admin on 2017/8/22.
+//  Copyright © 2017年 qurong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductBuyViewController : UIViewController
+
+@end
--- New-Project/qulicai/Controllers/Pruduct/ProductBuyViewController.m
+++ New-Project/qulicai/Controllers/Pruduct/ProductBuyViewController.m
@@ -1,36 +1,24 @@
 //
-//  AddBankCardViewController.m
+//  ProductBuyViewController.m
 //  qulicai
 //
 //  Created by admin on 2017/8/22.
 //  Copyright © 2017年 qurong. All rights reserved.
 //
 
-#import "AddBankCardViewController.h"
-#import "BankCardSelectedViewController.h"
 #import "ProductBuyViewController.h"
 
-@interface AddBankCardViewController ()
-
-@property (weak, nonatomic) IBOutlet UITextField *nameLabel;
-
-@property (weak, nonatomic) IBOutlet UITextField *bankCartLabel;
-
-@property (weak, nonatomic) IBOutlet UITextField *bankCartNumberLabel;
-
-@property (weak, nonatomic) IBOutlet UIButton *nextButton;
-
-@property (weak, nonatomic) IBOutlet UIView *bottomContainView;
+@interface ProductBuyViewController ()
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomViewBottomConstraint;
-
+@property (weak, nonatomic) IBOutlet UIView *bottomContainView;
+@property (weak, nonatomic) IBOutlet UIButton *bugButton;
+@property (weak, nonatomic) IBOutlet UITextField *moneyTextField;
 @property (weak, nonatomic) IBOutlet UILabel *alertErrorLabel;
-
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
-@implementation AddBankCardViewController
+@implementation ProductBuyViewController
 
 #pragma mark - lifeCycle
 
@@ -41,7 +29,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,11 +42,10 @@
     self.bottomContainView.hidden = YES;
     [self.view addTapGestureForDismissingKeyboard];
     [self setupNavigationItemLeft:[UIImage imageNamed:@"forget_back_image"]];
-    self.nameLabel.text = [NSString stringWithFormat:@"%@",self.name];
 }
 
 - (void)updateResetButtonStatus {
-    self.nextButton.enabled = self.bankCartNumberLabel.text.length;
+    self.bugButton.enabled = self.moneyTextField.text.length;
 }
 
 - (void)showErrorAlert {
@@ -82,21 +69,17 @@
     }];
 }
 
-- (void)save {
+- (void)buy {
     [self.view endEditing:YES];
     [self showSVProgressHUD];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [SVProgressHUD dismiss];
-        if (self.bankCartNumberLabel.text.length < 5) {
-            self.alertErrorLabel.text = @"银行卡号有误";
+        if ([self.moneyTextField.text floatValue] < 50) {
+            self.alertErrorLabel.text = @"购买失败";
             [self showErrorAlert];
         } else {
             //下一步操作
-            //[self showSuccessWithTitle:@"添加银行卡成功"];
-            ProductBuyViewController *productController = [[ProductBuyViewController alloc] init];
-            [self.navigationController pushViewController:productController
-                                                 animated:YES];
-        
+            [self showSuccessWithTitle:@"购买成功"];
         }
     });
 }
@@ -104,9 +87,9 @@
 #pragma mark - UITextViewDelegate
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    BOOL isFlag = self.bankCartNumberLabel.text.length;
+    BOOL isFlag = [self.moneyTextField.text floatValue] > 0;
     if (isFlag) {
-        [self save];
+        [self buy];
     }
     return YES;
 }
@@ -127,31 +110,11 @@
 }
 
 - (IBAction)save:(UIButton *)sender {
-    [self save];
+    [self buy];
 }
 
 - (void)leftBarButtonAction {
     [self.navigationController popViewControllerAnimated:YES];
 }
-
-- (IBAction)agreeProtocol:(UIButton *)sender {
-    NSLog(@"账户存管协议");
-}
-
-- (IBAction)applyProtocol:(UIButton *)sender {
-    NSLog(@"支付服务协议");
-}
-
-- (IBAction)selectBankButtonWasPressed:(UIButton *)sender {
-    BankCardSelectedViewController *bankCardController = [[BankCardSelectedViewController alloc] init];
-    bankCardController.bankBlock = ^(NSString *bankName, NSString *bankImageName) {
-        NSLog(@"name:::::%@::::::%@",bankName,bankImageName);
-        self.bankCartLabel.text = bankName;
-    };
-    [self.navigationController pushViewController:bankCardController
-                                         animated:YES];
-}
-
-
 
 @end
