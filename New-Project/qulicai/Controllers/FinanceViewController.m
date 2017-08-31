239bd7fc5cce39ff351681f124b0aecd6e5862b4
thomas <dui1cuo@126.com>
2017-08-31 15:17:04 +0800

    [Mod]minor fix

--- New-Project/qulicai/Controllers/FinanceViewController.m
+++ New-Project/qulicai/Controllers/FinanceViewController.m
@@ -174,9 +174,8 @@ UITableViewDataSource>
 - (NSArray *)imagesUrlString {
     if (!_imagesUrlString) {
         _imagesUrlString = @[
-                             @"fiscal_bg_image",
-                             @"https://ss0.baidu.com/-Po3dSag_xI4khGko9WTAnF6hhy/super/whfpf%3D425%2C260%2C50/sign=a41eb338dd33c895a62bcb3bb72e47c2/5fdf8db1cb134954a2192ccb524e9258d1094a1e.jpg",
-                             @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
+                             @"fiscal_bg1",
+                             @"fiscal_bg2",
                              ];
     }
     return _imagesUrlString;
